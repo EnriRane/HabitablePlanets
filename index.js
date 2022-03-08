5d196aef403245fef1b002dc72ba49fdc462f660
Enri Rane <enrirane@gmail.com>
2022-03-08 11:10:51 +0100

    First Kepler Data

---
+++ index.js
@@ -0,0 +1,28 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+fs.createReadStream("2.1 kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) habitablePlanets.push(data);
+  })
+  .on("error", (err) => {
+    console.log("An error happened!");
+  })
+  .on("end", () => {
+    console.log(` ${habitablePlanets.length} were found `);
+  });
